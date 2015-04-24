a8fc481a0be636eccb5909f357c6deef17ad7440
zhenglianfu <15067134439@163.com>
2015-04-24 17:51:33 +0800

    update

--- apps/lib/engine.js
+++ apps/lib/engine.js
@@ -1,10 +1,35 @@
 (function(){
-    var engine = {
-        sprite : function(opts) {
+    var join = Array.prototype.join;
+    // engine
+    function Engine(canvas){
+        this.ctx = canvas.getContext('2d');
+        // default style
+        this.fillStyle = '#fff';
+        this.strokeStyle = '#fff';
+        this.lineWidth = 1;
+        var buffer = document.createElement('canvas');
+        this.bufferCtx = buffer.getContext('2d');
+    }
+    Engine.prototype = {
+        setFillStyle : function(){
+            if (arguments.length == 1) {
+                this.ctx.fillStyle = arguments[0];
+            } else if (arguments.length >= 3) {
+                this.ctx.fillStyle = 'rgb(' + join.apply(arguments, [',']) + ')';
+            }
+        },
+        setStrokeStyle : function(){
+            if (arguments.length == 1) {
+                this.ctx.fillStyle = arguments[0];
+            } else if (arguments.length >= 3) {
+                this.ctx.fillStyle = 'rgb(' + join.apply(arguments, [',']) + ')';
+            }
+        },
+        moveTo : function(x, y){
+            this.ctx.moveTo(x, y);
+        },
+        drawRect : function(){
 
         }
-    };
-    engine.sprite.prototype = {
-
     };
 }());
