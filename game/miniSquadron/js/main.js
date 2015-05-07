e7916d5ed5e051fee1f2715625469f852c010766
zhenglianfu <15067134439@163.com>
2015-05-07 20:35:35 +0800

    update

--- game/miniSquadron/js/main.js
+++ game/miniSquadron/js/main.js
@@ -23,10 +23,21 @@
         containerCanvas = _id('containerCanvas'),
         directionCtrl = _id('directionCtrls'),
         w = document.body.clientWidth,
-        h = document.body.clientHeight;
+        h = document.body.clientHeight,
+        mouseDown = false;
     containerCanvas.width = bgCanvas.width = w;
     containerCanvas.height = bgCanvas.height = h;
+    directionCtrl.addEventListener('mousedown', function(e){
+        e.preventDefault();
+        mouseDown = true;
+    });
+    directionCtrl.addEventListener('mouseup', function(){
+        mouseDown = false;
+    });
     directionCtrl.addEventListener('mousemove', function(event){
+        if (!mouseDown) {
+            return;
+        }
         var x, y;
         if (event.pageX || event.pageY) {
             x = event.pageX;
@@ -42,6 +53,13 @@
         $('.direction').css("transform", 'rotate(' + (rotation / Math.PI * 180 + 135) + 'deg)');
         var py = player.speed * Math.sin(rotation);
         var px = player.speed * Math.cos(rotation);
-        console.log('x:' + px + ', y:' + py);
     }, false);
+    // TODO resize ...
+
+    // draw bg
+    var bgPainter = Canvas2D(bgCanvas);
+    var gradient  = bgPainter.getGradient(0, 0, 0, bgPainter.height);
+    gradient.addColorStop(0, '#444').addColorStop(50 / bgPainter.height, '#666').addColorStop(100 / bgPainter.height, '#5bf').addColorStop(1, '#4af');
+    bgPainter.rect(0,0,bgPainter.width,bgPainter.height);
+    bgPainter.fill(gradient.getGradient());
 })();
--- game/miniSquadron/js/util.js
+++ game/miniSquadron/js/util.js
@@ -53,3 +53,69 @@ _.extend(DomUtil, {
 
     }
 });
+// canvas simple package API
+var Canvas2D = function Canvas2D(canvas, opts){
+    if (!(this instanceof Canvas2D)) {
+        return new Canvas2D(canvas, opts);
+    }
+    this.canvas = canvas;
+    this.cxt    = canvas.getContext('2d');
+    this.opts = _.extend({
+        width  : canvas.width,
+        height : canvas.height
+    }, opts);
+    this.resize();
+}
+Canvas2D.prototype = {
+    get2DContext : function(){
+        return this.cxt;
+    },
+    resize : function() {
+        this.width  = this.canvas.width = this.opts.width;
+        this.height = this.canvas.height = this.opts.height;
+        return this;
+    },
+    translate : function(x, y){
+        this.ctx.translate(x, y);
+        return this;
+    },
+    clearRect : function(x, y, w, h){
+        this.ctx.clearRect(x, y, w, h);
+        return this;
+    },
+    fill : function(color){
+        if (color instanceof Gradient) {
+            color = color.getInstance();
+        }
+        this.cxt.fillStyle = color;
+        this.cxt.fill();
+    },
+    stroke : function(color){
+        this.cxt.strokeStyle = color;
+        this.cxt.stroke();
+    },
+    rect : function(x, y, w, h){
+        this.cxt.beginPath();
+        this.cxt.rect(x, y, w, h);
+    },
+    getGradient : function(sx, sy, ex, ey){
+        return Gradient(this.cxt.createLinearGradient(sx, sy, ex, ey), 'linear', this.canvas);
+    }
+};
+var Gradient = function Gradient(gradient, type, canvas){
+    if (!(this instanceof  Gradient)) {
+        return new Gradient(gradient, type, canvas);
+    }
+    this.instance = gradient;
+    this.type     = type;
+    this.parentCanvas = canvas;
+};
+Gradient.prototype = {
+    addColorStop : function(num, color){
+        this.instance.addColorStop(num, color);
+        return this;
+    },
+    getGradient : function(){
+        return this.instance;
+    }
+};
