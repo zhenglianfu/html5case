93878f2daf1d1ff72a9155528103a37e144e24b2
zhenglianfu <15067134439@163.com>
2015-05-25 21:47:13 +0800

    update

---
+++ javascript/animate.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2015/5/25.
+ */
+(function(window){
+    function HtmlAnimation(){
+
+    };
+    window.htmlAnimation = HtmlAnimation;
+}(window));
--- tools/css3Preview/js/tool_css3.js
+++ tools/css3Preview/js/tool_css3.js
@@ -1,7 +1,9 @@
 (function(){
     var menuClosed = true,
         $menu = $('.left-menu'),
-        $contents = $('.content-wrap');
+        $contents = $('.content-wrap'),
+        contentHeight = 0,
+        contentScrollTop = 0;
     $menu.find('.expand').click(function(){
         if (menuClosed) {
             $menu.addClass('open');
@@ -12,8 +14,19 @@
     });
     $menu.on('click', 'li', function(){
         var name = $(this).attr('data-alias');
-        $contents.find('.content').removeClass('active')
+        $contents.find('.content').removeClass('active');
         $contents.find('.' + name).addClass('active');
-
+        scrollToContent($contents.find('.' + name).index())
     });
+    function scrollToContent(index){
+        var $wrap = $('.content-wrap');
+
+    }
+    function resizePanel(){
+        var height = document.documentElement.clientHeight;
+        contentHeight = height - $('.footer').height();
+        $('.content-wrap, .content').height(contentHeight);
+    }
+    resizePanel();
+    window.addEventListener('resize', resizePanel);
 }());
