142a2199c48f06b94fe40f6e6de462c5db11680e
zhenglianfu <15067134439@163.com>
2015-07-22 21:34:42 +0800

    update css tools

--- tools/css3Preview/js/tool_css3.js
+++ tools/css3Preview/js/tool_css3.js
@@ -3,7 +3,8 @@
         $menu = $('.left-menu'),
         $contents = $('.content-wrap'),
         contentHeight = 0,
-        contentScrollTop = 0;
+        contentScrollTop = 0,
+        codePreWidth = 0;
     $menu.find('.expand').click(function(){
         if (menuClosed) {
             $menu.addClass('open');
@@ -26,17 +27,32 @@
             $menu.removeClass('open');
         }
     });
+    $('.bottom-expand').click(function(){
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $('.footer').height('1em');
+        } else {
+            $(this).addClass('open');
+            $('.footer').height('auto');
+        }
+        resizePanel();
+    });
     function scrollToContent(index){
-        $contents.scrollTop(contentHeight * index);
-
+        $contents.scrollTop(contentHeight * index)
     }
     function resizePanel(){
+        //// resize code width
+        codePreWidth = $('.result').width() - 80;
+        $('.code').width(codePreWidth);
+
         var height = document.documentElement.clientHeight;
-        contentHeight = height - $('.footer').height();
+        contentHeight = height - $('.footer')[0].clientHeight;
         $('.content-wrap, .content').height(contentHeight);
         // start point
         scrollToContent(0);
+
     }
     resizePanel();
     window.addEventListener('resize', resizePanel);
 }());
+// TODO 动态加入五个浏览器的codeline
