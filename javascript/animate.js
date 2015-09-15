f9ca012d89fd4559b157da9a53d5f60cc199b1c3
dingdang <dingdang@mamigumi.com>
2015-09-15 21:35:49 +0800

    modify css3_tool

--- javascript/animate.js
+++ javascript/animate.js
@@ -2,6 +2,30 @@
  * Created by Administrator on 2015/5/25.
  */
 (function(window){
+    var prefixs = ['webkit', 'moz', 'ms'],
+        lastTime = 0;
+    for (var i = 0, len = prefixs.length; i < len; i++) {
+        if (window.requestAnimationFrame && window.cancelAnimationFrame) {
+            break;
+        }
+        var prefix = prefixs[i];
+        window.requestAnimationFrame = window[prefix + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[prefix + 'CancelAnimationFrame'] || window[prefix + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!requestAnimationFrame || !cancelAnimationFrame) {
+        window.requestAnimationFrame = function(fn, ele){
+            var curTime = new Date().getTime(),
+                timeToCall = Math.max(0, 50 / 3 - (curTime - lastTime)),
+                id = setTimeout(fn, timeToCall);
+            lastTime = curTime + timeToCall;
+            return id;
+        }
+        window.cancelAnimationFrame = function(id){
+            window.clearTimeout(id);
+        }
+    }
+
     function HtmlAnimation(){
 
     };
--- tools/css3Preview/js/flexbox.js
+++ tools/css3Preview/js/flexbox.js
@@ -2,7 +2,22 @@
  * Created by zhenglianfu on 2015/9/12.
  */
 
+// set random color
+function randomColor(){
+    var r = Math.random() * 255 | 0,
+        g = Math.random() * 255 | 0,
+        b = Math.random() * 255 | 0;
+    console.log(r,g,b);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
 $('#flexBtn').bind('click', function(){
     var params = form2Obj($('#flexBox .form'));
     console.log(params);
+});
+
+$('.flex-box').children().each(function(){
+    $(this).css({
+        'background': randomColor()
+    });
 });
